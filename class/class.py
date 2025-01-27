ca219a62f319b1b38dc83f1ad023d5b702d490b1
Miss-mohlaroy <missmahmudova88@icloud.com>
2025-01-27 10:36:05 +0500

    mmm

--- class/class.py
+++ class/class.py
@@ -134,9 +134,128 @@ class Student():
     def get_age(self, now=2025):
         """  """
         return now - self.year
+    
 
-student1 = Student("Mohlaroy", "Mahmudova", 9, [5,5,5,5,5]), ["Oydina","Rahima","Naima"]
-print(student1.get_info())
-print(student1.orta_baho())
+    def set_grade(self):
+        """ Talabaning kursini oshiruvchi funksiya (1-6)"""
+        if self.grade < 6:
+            self.grade += 1
+        else:
+            return "Siz hozirda eng so'ngi kursda siz!!"
+        
+        return self.grade
+    
+
+    def get_grade(self):
+        """  """
+        return self.grade 
+
+# student1 = Student("Mohlaroy", "Mahmudova", 9, [5,5,5,5,5]), ["Oydina","Rahima","Naima"]
+
+
+# print(student1.get_info())
+# print(student1.orta_baho())
+# print(student1.set_grade())
+# print(student1.set_grade())
+# print(student1.set_grade())
+# print(student1.set_grade())
+# print(student1.set_grade())
+# print(student1.set_grade())
+
+
+
+
+""" book class"""
+class Book():
+    """"""
+    def __init__(self, name:str, author:str, year: int, publisher:str, price:int):
+        """ """
+        self.name = name
+        self.author = author
+        self.year = year
+        self.publisher = publisher
+        self.price = price
+
+
+    def __str__(self):
+        return self.name
+
+
+    def get_info(self):
+        return f"Kitobning malumotlari: \nNomi: {self.name} \nAvtor: {self.author} \nYil: {self.year} \nPublisher: {self.publisher} \nNarx: {self.price} $"
+
+    def get_name(self):
+        return self.name
+        
+    def get_price(self):
+        return self.price
+
+
+book1 = Book("nimadir", "kimdir", 2012, "yana kimdir", 50_000)
+
+print(book1)
+
+
+print(book1.get_info())
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" kutubxona """
+class Library():
+    """ Kutubxona klassi"""
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.books = []
+        self.books_count = 0
+
+    def __str__(self):
+        return self.name
+
+    # def __repr__(self):
+    #     return self.name
+
+
+    def get_info(self):   
+        """ Kutubxona haqidagi malumotlar"""
+        return f"Kutubxona nomi: {self.name}, \nmanzil: {self.address}, \nqoshilgan kitoblar soni: {self.books_count}"
+
+
+    def add_book(self, book):
+        """ Kutubxonaga kitob qoshuvchi funksiya """
+        self.books.append(book)
+        self.books_count += 1
+    
+        return self.books
+
+
+    def get_book(self):
+        """ Kutubxonadagi kitoblar nomini qaytaradigan funksiya """
+        info = f"{self.address} kutubxonasi, \nmanzil: "
+        for book in self.books:
+            info += f"{book}, "
+
+        return info
+
+
+kitob1 = Library("Nodira Begim", "lola")
+
+
+# print(kitob1.add_book("yasha"))
+# print(kitob1.get_book())
+# print(kitob1.get_info())
+
+# print(kitob1)
+
 
 
